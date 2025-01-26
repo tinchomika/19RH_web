04e8f5c8c5bad5c9b862255bb6c422bde087d2f8
tinchomika <ricardito97@hotmail.com>
2025-01-26 18:04:43 -0300

    borrada la basura, solucionado bug del script de scroll y agregada la sección donaciones

--- webv3/scroll.js
+++ webv3/scroll.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
+$(".header a").on('click', function(event) {
         // Make sure this.hash has a value before overriding default behavior
         if (this.hash !== "") {
             // Prevent default anchor click behavior
